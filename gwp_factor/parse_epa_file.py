ef8a450d5a6f82ea6c5f11c379f43973bf7ba413
peter279k <peter279k@gmail.com>
2022-05-19 18:29:23 +0800

    Add fugitive emission parser and CSV datasets

--- gwp_factor/parse_epa_file.py
+++ gwp_factor/parse_epa_file.py
@@ -18,10 +18,26 @@ def parse_ods_file(ods_file_path):
 
     ods_data = get_data(ods_file_path)
     ods_sheet_keys = list(ods_data.keys())
+
+    print('Parsing the ' + ods_sheet_keys[1] + ' is started.')
     parse_co2_burn(ods_data, ods_sheet_keys[1], csv_dir)
+    print('Parsing the ' + ods_sheet_keys[1] + ' is done.')
+
+    print('Parsing the ' + ods_sheet_keys[2] + ' is started.')
     parse_ch4_n2o_burn(ods_data, ods_sheet_keys[2], csv_dir)
+    print('Parsing the ' + ods_sheet_keys[2] + ' is done.')
+
+    print('Parsing the ' + ods_sheet_keys[3] + ' is started.')
     parse_ch4_n2o_burn(ods_data, ods_sheet_keys[3], csv_dir)
+    print('Parsing the ' + ods_sheet_keys[3] + ' is done.')
+
+    print('Parsing the ' + ods_sheet_keys[4] + ' is started.')
     parse_cfcs_gwp(ods_data, ods_sheet_keys[4], csv_dir)
+    print('Parsing the ' + ods_sheet_keys[4] + ' is done.')
+
+    print('Parsing the ' + ods_sheet_keys[6] + ' is started.')
+    parse_fugitive_emission(ods_data, ods_sheet_keys[6], csv_dir)
+    print('Parsing the ' + ods_sheet_keys[6] + ' is done.')
 
 
 def parse_co2_burn(ods_data, ods_burn_sheet_key, csv_dir):
@@ -87,6 +103,7 @@ def parse_cfcs_gwp(ods_data, ods_burn_sheet_key, csv_dir):
     comment_index = ods_heads.index('備註')
     csv_head = ','.join(ods_heads[1:comment_index]) + '\n'
     csv_file_path = csv_dir + '/' + ods_burn_sheet_key + '.csv'
+
     index = 2
     csv_rows = csv_head
     while index < len(ods_data[ods_burn_sheet_key]):
@@ -104,6 +121,23 @@ def parse_cfcs_gwp(ods_data, ods_burn_sheet_key, csv_dir):
 
     return True
 
+
+def parse_fugitive_emission(ods_data, ods_burn_sheet_key, csv_dir):
+    csv_head = '設備名稱,排放因子(％),防治設備回收率(％),防治設備使用率(％),冷媒排放係數,單位,來源\n'
+    csv_file_path = csv_dir + '/' + ods_burn_sheet_key + '.csv'
+    index = 237
+    end_index = 244
+    csv_rows = csv_head
+    while index <= end_index:
+        mapped = list(map(str, ods_data[ods_burn_sheet_key][index][1:]))
+        csv_rows += ','.join(mapped[0:1] + mapped[2:]) + '\n'
+        index += 1
+
+    csv_handler = open(csv_file_path, 'w')
+    csv_handler.write(csv_rows)
+    csv_handler.close()
+
+    return True
 
 
 ods_files = glob.glob('./datasets/*.ods')
