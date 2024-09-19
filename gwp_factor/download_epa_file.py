b416d176207a7043350acf495e899ff84ade916e
peter279k <peter279k@gmail.com>
2024-09-19 20:29:59 +0800

    Fix download_epa_file

--- gwp_factor/download_epa_file.py
+++ gwp_factor/download_epa_file.py
@@ -5,20 +5,18 @@ from bs4 import BeautifulSoup
 
 
 print('Download latest GWP factor file from EPA is started')
-file_url = 'https://ghgregistry.epa.gov.tw/ghg_rwd/Main/Tool/Tool_1?Type=1'
-download_file_url = 'https://ghgregistry.epa.gov.tw/upload/Tools/'
+file_url = 'https://ghgregistry.moenv.gov.tw/epa_ghg/Downloads/FileDownloads.aspx?Type_ID=1'
+prefix_download_file_url = 'https://ghgregistry.moenv.gov.tw/upload/Tools/'
 
 response = requests.get(file_url)
 soup = BeautifulSoup(response.text, 'html.parser')
-spans = soup.select('span')
+spans = soup.select('table[id="CPHCont_GV"] > tr > td > a')
 keyword = '溫室氣體排放係數管理表'
 ods_keyword = 'ODS檔'
 download_files = []
 for span in spans:
-    if span.string is None:
-        continue
-    if keyword in span.string and ods_keyword in span.string:
-        ods_filename = span.string.replace('（ODS檔）', '')
+    if keyword in span['title'] and ods_keyword in span['title']:
+        ods_filename = span.string.replace('(ODS檔)', '')
         ods_filename += '.ods'
         if os.path.isfile('./datasets/' + ods_filename) is False:
             print(ods_filename + ' file is not found.')
@@ -26,7 +24,7 @@ for span in spans:
 
 
 for download_file in download_files:
-    response = requests.get(download_file_url + download_file)
+    response = requests.get(prefix_download_file_url + download_file)
     f_handler = open('./datasets/' + download_file, 'wb')
     for chunk in response.iter_content():
         if chunk:
